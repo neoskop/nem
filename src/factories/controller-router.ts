1f3cb92d35f4798fe90af3afc9b0589894101bc6
Mark Wecke <wecke@neoskop.de>
2018-03-05 15:01:42 +0100

    feat(): add controller handling

--- src/factories/controller-router.ts
+++ src/factories/controller-router.ts
@@ -1,13 +1,28 @@
 import { Annotator, Type } from '../utils/annotations';
 import { NextFunction, Router, Request, Response } from 'express';
-import { AbstractController, AbstractMethod } from '../metadata/controller';
+import { AbstractController, AbstractMethod, ApplicableAnnotation } from '../metadata/controller';
 import { Injector, InjectorFactory, Injectable, Provider } from '@neoskop/injector';
 import { AbstractParam } from '../metadata/params';
 import { ParamFactory } from './param';
 import { NemRootZone } from '../zone';
 import { copyMultiProvider } from '../utils/misc';
-import { MULTI_TOKENS_FROM_PARENT } from '../tokens';
+import { DEFAULT_END_HANDLER, MULTI_TOKENS_FROM_PARENT } from '../tokens';
+import { Result } from '../metadata/result';
+
 const debug = require('debug')('nem:factory:controller');
+
+export interface MethodAnnotationMap extends Map<Type<any>, any> {
+    get(key : typeof AbstractMethod) : AbstractMethod[];
+    get(key : typeof ApplicableAnnotation) : ApplicableAnnotation[];
+}
+
+export interface IControllerContext {
+    cls : Type<any>;
+    metadata : AbstractController;
+    injector : Injector;
+    instance : any;
+    methods : Map<string, MethodAnnotationMap>;
+}
 
 @Injectable()
 export class ControllerRouterFactory {
@@ -17,33 +32,40 @@ export class ControllerRouterFactory {
                 protected paramFactory : ParamFactory) {
     }
     
-    createRouterFromController(cls : Type<any>, { providers = [] } : { providers?: Provider[] } = {}) : Router {
+    createRouterFromController(cls : Type<any>, { providers = [] } : { providers? : Provider[] } = {}) : Router {
         debug('create router from controller', cls.name);
         const router = Router();
-    
-        const controller = this.assertControllerAnnotation(cls);
-        const injector = InjectorFactory.create({
-            parent: this.injector,
-            providers: [
-                ...copyMultiProvider(MULTI_TOKENS_FROM_PARENT, this.injector),
-                ...providers,
-                ...(controller.providers || []),
-                cls
-            ]
-        });
-        const instance = injector.get(cls);
-        const methods = this.createMethodMap(cls);
         
-        for(const [ method, annotations ] of methods) {
+        const ctx = this.initContext(cls, providers);
+        
+        for(const [ method, annotations ] of ctx.methods) {
             const params = this.getParamsForMethod(cls, method);
             
-            for(const annotation of annotations) {
-                router[annotation.method](annotation.path, this.createHandler(instance, method, params))
+            for(const annotation of annotations.get(AbstractMethod)) {
+                router[ annotation.method ](annotation.path, this.createHandler(ctx, method, params, annotations))
             }
         }
         
         
         return router;
+    }
+    
+    protected initContext(cls : Type<any>, providers : Provider[]) : IControllerContext {
+        const ctx : Partial<IControllerContext> = { cls };
+        ctx.metadata = this.assertControllerAnnotation(cls);
+        ctx.injector = InjectorFactory.create({
+            parent   : this.injector,
+            providers: [
+                ...copyMultiProvider(MULTI_TOKENS_FROM_PARENT, this.injector),
+                ...providers,
+                ...(ctx.metadata.providers || []),
+                cls
+            ]
+        });
+        ctx.instance = ctx.injector.get(cls);
+        ctx.methods = this.createMethodAnnotationMap(cls);
+        
+        return ctx as IControllerContext;
     }
     
     protected assertControllerAnnotation(cls : Type<any>) : AbstractController {
@@ -58,18 +80,22 @@ export class ControllerRouterFactory {
         throw new Error(`Class "${cls.name}" has no controller annotation`);
     }
     
-    protected createMethodMap(cls : Type<any>) : Map<string, AbstractMethod[]> {
+    protected createMethodAnnotationMap(cls : Type<any>) : Map<string, MethodAnnotationMap> {
         const annotations = Annotator.getPropAnnotations(cls);
-        const methods = new Map<string, AbstractMethod[]>();
-    
+        const methods = new Map<string, MethodAnnotationMap>();
+        
+        const types : Type<any>[] = [ AbstractMethod, ApplicableAnnotation ] as any[];
         
         for(const prop in annotations) {
-            for(const annotation of annotations[prop]!) {
-                if(annotation instanceof AbstractMethod) {
-                    if(methods.has(prop)) {
-                        methods.get(prop)!.push(annotation);
-                    } else {
-                        methods.set(prop, [ annotation ]);
+            methods.set(prop, new Map());
+            for(const annotation of annotations[ prop ]!) {
+                for(const type of types) {
+                    if(annotation instanceof type) {
+                        if(!methods.get(prop)!.has(type)) {
+                            methods.get(prop)!.set(type, [ annotation ]);
+                        } else {
+                            methods.get(prop)!.get(type)!.push(annotation);
+                        }
                     }
                 }
             }
@@ -91,16 +117,16 @@ export class ControllerRouterFactory {
                 throw new Error(`Too many param annotations for param ${index} of method ${cls.name}:${method}`);
             }
             
-            result.push(p[0]);
+            result.push(p[ 0 ]);
         }
         
         return result;
     }
     
-    protected createHandler(instance : any, method : string, params : AbstractParam[]) {
+    protected createHandler(ctx : IControllerContext, method : string, params : AbstractParam[], annotations : MethodAnnotationMap) {
         return async (request : Request, response : Response, next : NextFunction) => {
             const zone = this.zone.fork({
-                name: '',
+                name      : '',
                 properties: {
                     request,
                     response
@@ -109,16 +135,52 @@ export class ControllerRouterFactory {
             
             zone.run(async () => {
                 try {
+                    const appAnnotations = annotations.get(ApplicableAnnotation) || [];
+                    
+                    await applyApplicableAnnotations(appAnnotations, 'before', { request, response });
+                    
                     const args = await Promise.all(params.map(param => this.paramFactory.getParameterFromMetadataAndRequest(param, request)));
                     
-                    const result = await instance[ method ](...args);
-        
-                    response.end(result);
+                    const result = Result.ensure(await ctx.instance[ method ](...args));
+                    
+                    await applyApplicableAnnotations(result.args, 'before', { request, response });
+                    
+                    appAnnotations.push(...result.args);
+                    
+                    await applyApplicableAnnotations(appAnnotations, 'after', {
+                        request,
+                        response,
+                        result: result.result
+                    });
+                    await applyApplicableAnnotations([
+                        ...appAnnotations,
+                        ctx.injector.get(DEFAULT_END_HANDLER, {
+                            end() {
+                                throw new Error('Missing end annotation')
+                            }
+                        })
+                    ], 'end', { request, response, result: result.result }, { stopAfterFirst: true });
+                    
                 } catch(e) {
+                    console.log('catch', e);
                     next(e);
                 }
             });
             
+        }
+    }
+}
+
+async function applyApplicableAnnotations(app : ApplicableAnnotation[],
+                                          type : 'before' | 'after' | 'end',
+                                          args : { request : Request, response : Response, result? : any },
+                                          { stopAfterFirst } : { stopAfterFirst? : boolean } = {}) {
+    for(const a of app) {
+        if(a[ type ]) {
+            await (a[ type ] as any)(a, args);
+            if(stopAfterFirst) {
+                return;
+            }
         }
     }
 }
--- src/tokens.ts
+++ src/tokens.ts
@@ -1,10 +1,12 @@
 import { InjectionToken } from '@neoskop/injector'
 import { ErrorRequestHandler } from 'express';
+import { ApplicableAnnotation } from './metadata/controller';
 
 export const BASE_PATHS = new InjectionToken<(string|RegExp)[]>('BasePaths');
 export const ERROR_HANDLER = new InjectionToken<ErrorRequestHandler>('ErrorHandler');
 export const VIEWS = new InjectionToken<string|string[]>('Views');
 export const VIEW_ENGINE = new InjectionToken<string>('View Engine');
+export const DEFAULT_END_HANDLER = new InjectionToken<ApplicableAnnotation>('Default End Handler');
 
 export const MULTI_TOKENS_FROM_PARENT = [
     BASE_PATHS
