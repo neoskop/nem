79d345f3486df7b11e3b4fe7b936759b51c83217
Mark Wecke <wecke@neoskop.de>
2018-11-12 14:36:18 +0100

    fix: fix typings issue in module factories sample

--- samples/04-module-factories/modules/index/module.ts
+++ samples/04-module-factories/modules/index/module.ts
@@ -1,6 +1,6 @@
-import { NemModule, NemModuleWithProviders } from '../../../../src/metadata/module';
-import { IndexController } from './controller/index';
 import { InjectionToken } from '@neoskop/injector';
+import { NemModule } from '../../../../src/metadata/module';
+import { IndexController } from './controller/index';
 
 export const DATA = new InjectionToken<{ id: string }[]>('Rest Data');
 
